b52dbe0c300de2a3a1f2212172bb12051402ac52
Victor <nastoyashchiy@gmail.com>
2023-03-31 19:40:19 +0300

    css update

--- code.js
+++ code.js
@@ -1,7 +1,7 @@
 const API_URL = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json';
 
 let currencyData = null;
-
+/* fetch part*/
 function getData() {
   fetch(API_URL)
   .then(response => {
@@ -12,19 +12,19 @@ function getData() {
     }
   })
   .then(data => {
-    const uahData = { // create new object for UAH
+    const uahData = { /* creating UAH object*/
       r030: 36,
       txt: ' Українська гривня',
       rate: 1,
       cc: 'UAH',
       exchangedate: data[0].exchangedate,
-      exchangeRate: 1 // set exchange rate to 1
+      exchangeRate: 1
     };
     currencyData = data.slice().map(curr => ({
       ...curr,
       exchangeRate: curr.rate / curr.rate
-    })); // Only display the first 10 currencies for brevity
-
+    }));
+    /* adding UAH object, deleting rub and byn, sorting by ABC */
     currencyData.unshift(uahData);
     currencyData = currencyData.filter(curr => curr.cc !== 'RUB' && curr.cc !== 'BYN');
     currencyData.sort((a, b) => {
@@ -46,7 +46,7 @@ function getData() {
   });
 
 }
-
+/*rendering first part*/
 function renderPart1(currency) {
   const currencyList = document.querySelector('#currencyList');
   const targetCurrencyList = document.querySelector('#targetCurrencyList');
@@ -75,14 +75,14 @@ function renderPart1(currency) {
     }
   });
 }
-
+/*rendering second part*/
 function renderPart2(fromCurrencyCode, toCurrencyCode, exchangeRate) {
   const part2Div = document.querySelector('#part2');
   if (part2Div) {
     part2Div.innerHTML = `
       <h2>${fromCurrencyCode} to ${toCurrencyCode} Rate: ${exchangeRate}</h2>
     `;
-  } else {
+  }else {
     const part1Div = document.querySelector('#part1');
     part1Div.innerHTML += `
       <div id="part2">
@@ -93,180 +93,3 @@ function renderPart2(fromCurrencyCode, toCurrencyCode, exchangeRate) {
 }
 
 getData();
-
-
-
-
-
-
-
-
-/*const url='https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json/'
-let characters = []
-
-fetch(url, {
-  method: 'GET',
-}).then(function(response){
-    return response.json()
-}).then(function(data){
-    characters = data
-    console.log(characters)  
-    renderCharacters(app, characters)
-})
-
-function renderCharacters(app, characters){ 
-  console.log(characters) 
-  const part1Div = document.querySelector('#app');
-  part1Div.innerHTML = `
-  <section >
-        <label for="money">What type ?</label>
-        <input list="moneys" id="money" name="money">
-        <datalist id="moneys">
-        <option id="part2"></option> 
-        </datalist>
-      </section>
-  `
-  const part2Div = document.querySelector('#part2');
-  part2Div.innerHTML = `
-  ${character.txt}`
-    for(const character of characters){
-        const characterContainer = document.createElement('div')
-        characterContainer.classList.add('container')
-        characterContainer.innerHTML = `
-        d
-
-        <!-- ${character.txt}(${character.cc}) =  ${character.rate} 
-        
-        <section >
-        <label for="money">What type ?</label>
-        <input list="moneys" id="money" name="money">
-        <datalist id="moneys">
-          <option value="${character.cc}"></option>
-        </datalist>
-      </section>
-        
-        --> 
-        `
-        app.appendChild(characterContainer)
-    }
-}
-
-const app = document.getElementById('app')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-const url='https://swapi.dev/api/people/' // 1
-let characters = [] // 2
-
-fetch(url, { // 3
-  method: 'GET',
-}).then(function(response){
-    return response.json() // 6
-}).then(function(data){
-    characters = data.results // 7
-    renderCharacters(app, characters) // 8
-})
-
-// 4 function initialization
-function renderCharacters(app, characters){ 
-    for(const character of characters){ // 9
-        const characterContainer = document.createElement('div') // 9.1
-        characterContainer.classList.add('container')
-        characterContainer.innerHTML = `
-            <p class="name"> ${character.name} </p>
-            <p> Height: ${character.height}cm </p>
-            <p> Mass: ${character.mass}kg </p>
-            <p> Hair: ${character.hair_color}</p>
-            <p> Skin: ${character.skin_color}</p>
-            <p> Eye color: ${character.eye_color}</p>
-            <p> Birth year: ${character.birth_year}</p>
-            <p> Gender: ${character.gender}</p>
-            <p id="homeworld"> Homeworld: </p>
-            <p> Films: ${character.films[0]}</p>
-            <p> Species: ${character.species}</p>
-            <p> Vehicles:  </p>
-            <p> Starships: ${character.starships[0]}</p>
-        `
-        app.appendChild(characterContainer) // 9.8
-    }   
-}
-
-const app = document.getElementById('app') // 5
-
-/*
-- creating currency list in HTML -v
-- creating connection with html and js values -v
-- fetch with currency API:                  -x
-1 adding link
-https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json/
-another API:
-https://api.apilayer.com/exchangerates_data/convert?to=usd&from=uah&amount=37
-2 adding event listeners 
-
-*/
-/*
-
-const currencyEl_one = document.getElementById('currency-one');
-const currencyEl_two = document.getElementById('currency-two');
-const amountEl_one = document.getElementById('amount-one');
-const amountEl_two = document.getElementById('amount-two');
-
-const rateEl = document.getElementById('rate');
-const swap = document.getElementById('swap');
-
-
-
-
-function calculate(){
-    const currency_one = currencyEl_one.value;
-    const currency_two = currencyEl_two.value;
-
-    fetch(`https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json/`)
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data);
-        })
-}
-
-/*currencyEl_one.addEventListener('change', calculate); ?????sees mistake
-currencyEl_two.addEventListener('change', calculate);
-amountEl_one.addEventListener('input', calculate);
-amountEl_two.addEventListener('input', calculate);
-
-calculate();
-const app = document.getElementById('app')
-
-
-
-/*
-another way
-
-
-var myHeaders = new Headers();
-myHeaders.append("apikey", "xPQxycteiclKxyIaNHGowCqU5EYAJjqa");
-
-var requestOptions = {
-  method: 'GET',
-  redirect: 'follow',
-  headers: myHeaders
-};
-console.log(myHeaders)
-fetch("https://api.apilayer.com/exchangerates_data/convert?to=usd&from=uah&amount=37", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
-  
-*/
--- index.js
+++ index.js
@@ -2,15 +2,20 @@ const startAnimationBtn = document.getElementById('start-animation-btn');
 const background = document.querySelector('.background');
 const audio = document.getElementById("myAudio");
 
+const navElement = document.querySelector("nav");
+const mainElement = document.querySelector("main");
+const gitElement = document.querySelector("#git_btn");
+const cvElement = document.querySelector("#cv_btn");
+const wgElement = document.querySelector("#wg_btn");
+
 startAnimationBtn.addEventListener('click', function() {
   background.classList.remove('hidden')
-   audio.play();
+  audio.play();
+  navElement.style.backgroundColor = "#023020";
+  mainElement.style.backgroundColor = "#023020";  
+  gitElement.style.backgroundColor = "#023020";  
+  cvElement.style.backgroundColor = "#023020";
+  wgElement.style.backgroundColor = "#023020";
+  startAnimationBtn.style.backgroundColor = "#023020";
+
 });
-
-
-/*
-const button = document.getElementById("start-animation-btn");
-
-button.addEventListener("click", function() {
- ;
-});*/
